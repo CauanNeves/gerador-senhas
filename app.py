ed1dd2fdf5a4a29beef44832d462a37d736489da
Cauan Neves <c.neves8903@gmail.com>
2024-12-23 19:40:28 -0300

    1.0

--- app.py
+++ app.py
@@ -2,53 +2,60 @@ import PySimpleGUI as sg
 import random
 import string
 
-#Tema
+# Tema
 sg.theme('DarkBlue17')
 
-#Layout
-#Parte das senhas geradas
-frame_result= [
-    [sg.Text('', key= 'result1'), sg.Text('', key= 'result4')],
-    [sg.Text('', key= 'result2'), sg.Text('', key= 'result5')],
-    [sg.Text('', key= 'result3'), sg.Text('', key= 'result6')],
+# Layout
+# Parte das senhas geradas
+frame_result = [
+    [sg.Text('', key='result1', justification= 'c', expand_x= True), sg.Text('', key='result4', justification= 'c', expand_x= True)],
+    [sg.Text('', key='result2', justification= 'c', expand_x= True), sg.Text('', key='result5', justification= 'c', expand_x= True)],
+    [sg.Text('', key='result3', justification= 'c', expand_x= True), sg.Text('', key='result6', justification= 'c', expand_x= True)],
 ]
 
-#Layout principal
-main_layout= [
+# Layout principal
+main_layout = [
     [sg.Text('Comprimento da Senha:')],
-    [sg.Slider(range= (4, 15), orientation= 'h', key= 'comprimento')],
+    [sg.Slider(range=(4, 15), default_value=8, orientation='h', key='comprimento')],
     [sg.Text('Componentes:')],
-    [sg.Checkbox('Maiúscula', key= 'maiuscula'), sg.Checkbox('Minúscula', key= 'minuscula')],
-    [sg.Checkbox('Números', key= 'numeros'), sg.Checkbox('Símbolos', key= 'simbolos')],
-    [sg.Button('Gerar', key= 'btn-gerar')],
-    [sg.Frame('Senhas', frame_result, expand_x= True)]
+    [sg.Checkbox('Maiúscula', key='maiuscula'), sg.Checkbox('Minúscula', key='minuscula')],
+    [sg.Checkbox('Números', key='numeros'), sg.Checkbox('Símbolos', key='simbolos')],
+    [sg.Button('Gerar', key='btn-gerar')],
+    [sg.Frame('Senhas', frame_result, expand_x=True)],
 ]
 
-#Janela
-window= sg.Window('Gerador de senha', layout= main_layout, element_justification= 'c', size= (450, 300))
-#Leitura de eventos e valores
+# Janela
+window = sg.Window('Gerador de Senha', layout=main_layout, element_justification='c', size=(400, 310))
+
+# Função para gerar senha
+def gerar_senha(comprimento, maiuscula, minuscula, numeros, simbolos):
+    caracteres = ''
+    if maiuscula:
+        caracteres += string.ascii_uppercase
+    if minuscula:
+        caracteres += string.ascii_lowercase
+    if numeros:
+        caracteres += string.digits
+    if simbolos:
+        caracteres += '@#%&$.'
+
+    if not caracteres:
+        return 'Selecione pelo menos uma opção'
+    
+    return ''.join(random.choice(caracteres) for _ in range(int(comprimento)))
+
+
+# Loop de eventos
 while True:
-    event, values= window.read()
-    #ler e reagir
+    event, values = window.read()
     if event == sg.WIN_CLOSED:
         break
     elif event == 'btn-gerar':
-        #Senha com letras maiúsculas, sem números e sem caracteres especiais
-        if values['maiuscula'] == True and values['minuscula'] == True:
-            senha = ''
-            for i in range(int(values['comprimento'])):
-                senha += random.choice(string.ascii_letters) #Letras maiúsculas e minúsculas
-            print(senha)
-        #Senha minúscula sem números e sem caracteres especiais
-        elif values['minuscula'] == True and values['maiuscula'] == False:
-            senha = ''
-            for i in range(int(values['comprimento'])):
-                senha += str.lower(random.choice(string.ascii_letters)) #Letras minúsculas
-            print(senha)
-        #Senha com números, letras maiúsculas e minúsculas e sem caracteres especiais
-        elif values['numeros'] == True:
-            senha= ''
-            for i in range(int(values['comprimento'])):
-                senha += random.choice(string.hexdigits) #Letras e números
-            print(senha)
-        print(event, values)
+        senhas = [gerar_senha(values['comprimento'], values['maiuscula'], values['minuscula'], values['numeros'], values['simbolos']) for _ in range(6)]
+        
+        # Atualiza os campos de texto com as senhas geradas
+        for i, senha in enumerate(senhas):
+            window[f'result{i+1}'].update(senha)
+
+# Fecha a janela
+window.close()
